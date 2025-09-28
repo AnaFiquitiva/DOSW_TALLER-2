1e24862ce29fbd6afb0ab024aa90cd22b333f6d7
unknown <valeria.bermudez-a@mail.escuelaing.edu.co>
2025-09-28 15:30:29 -0500

    complementando la clase transaccion

--- src/main/java/edu/dosw/taller/DOSW_TALLER_2/model/Transaccion.java
+++ src/main/java/edu/dosw/taller/DOSW_TALLER_2/model/Transaccion.java
@@ -3,6 +3,7 @@ package edu.dosw.taller.DOSW_TALLER_2.model;
 import lombok.Data;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 public class Transaccion {
@@ -11,4 +12,49 @@ public class Transaccion {
     private BigDecimal monto;
     private LocalDate fecha;
     private String categoria;
+
+    public Transaccion() {}
+    public Transaccion(String id, String descripcion, BigDecimal monto, LocalDate fecha, String categoria) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.monto = monto;
+        this.fecha = fecha;
+        this.categoria = categoria;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public BigDecimal getMonto() {
+        return monto;
+    }
+    public void setMonto(BigDecimal monto) {
+        this.monto = monto;
+    }
+    public LocalDate getFecha() {
+        return fecha;
+    }
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+
 }
+
+
+
